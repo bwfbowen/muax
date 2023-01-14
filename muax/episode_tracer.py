d4abfe1285e9e8bad9dafed8c33ca2f1ed3545f6
bwfbowen <104526323+bwfbowen@users.noreply.github.com>
2023-01-14 17:27:29 -0500

    Update episode_tracer.py

--- muax/episode_tracer.py
+++ muax/episode_tracer.py
@@ -3,6 +3,7 @@
 
     Copyright (c) 2020 Microsoft Corporation.
     Copyright (c) 2021 github.com/coax-dev
+    Copyright (c) 2022 bf2504@columbia.edu
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
