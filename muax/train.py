f7bb774883537fb1961c95905291dac1f7c3640e
bwfbowen <bf2504@columbia.edu>
2023-01-24 21:15:37 +0000

    train module

--- muax/train.py
+++ muax/train.py
@@ -13,6 +13,7 @@ def train(model, env_id,
           tracer=PNStep(50, 0.997, 0.5), 
           buffer=TrajectoryReplayBuffer(500),
           max_episodes: int = 1000, 
+          save_every_n_eps: int = 50,
           num_simulations: int = 50,
           k_steps: int = 10,
           buffer_warm_up: int = 128,
@@ -20,11 +21,14 @@ def train(model, env_id,
           sample_per_trajectory: int = 10,
           name: str = None,
           tensorboard_dir=None, 
+          save_path=None,
           random_seed: int = 42):
   if name is None:
     name = env_id 
   if tensorboard_dir is None:
-    tensorboard_dir = './'
+    tensorboard_dir = '.'
+  if save_path is None:
+    save_path = 'model_params'
   env = gym.make(env_id, render_mode='rgb_array')
   env = coax.wrappers.TrainMonitor(env, name=name, tensorboard_dir=os.path.join(tensorboard_dir, name))
 
@@ -42,7 +46,7 @@ def train(model, env_id,
                            with_pi=True, 
                            with_value=True, 
                            obs_from_batch=False,
-                           num_simulations=50)
+                           num_simulations=num_simulations)
       obs_next, r, done, truncated, info = env.step(a)
       if truncated:
         r = 1 / (1 - tracer.gamma)
@@ -61,4 +65,7 @@ def train(model, env_id,
                                          k_steps=k_steps)
         metrics = model.update(transition_batch)
         env.record_metrics(metrics)
+    if ep % save_every_n_eps == 0:
+      model.save(save_path)
+  return model
     
