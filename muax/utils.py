bfe108be5e14dc90b0867aa42b741fa490e9d06f
bwfbowen <bf2504@columbia.edu>
2023-02-20 23:53:45 +0000

    l2 regulariser

--- muax/utils.py
+++ muax/utils.py
@@ -21,10 +21,33 @@ jax.tree_util.register_pytree_node(
   unflatten_func=lambda treedef, leaves: sliceable_deque(leaves)
 )
 
+
 def scale_gradient(g, scale: float = 1):
     return g * scale + jax.lax.stop_gradient(g) * (1. - scale)
+
 
 def min_max(state):
     _min = jax.lax.stop_gradient(jnp.min(state))
     _max = jax.lax.stop_gradient(jnp.max(state))
     return (state - _min) / (_max - _min)   
+
+
+def _scaling(x, eps: float = 1e-3):
+  """Reference: https://arxiv.org/abs/1805.11593"""
+  return jnp.sign(x) * (jnp.sqrt(jnp.abs(x) + 1) - 1) + eps * x
+
+
+def _inv_scaling(x, eps: float = 1e-3):
+  """The inverse scaling of the output of `_scaling`"""
+  return jnp.sign(x) * (
+        ((jnp.sqrt(1 + 4 * eps * (jnp.abs(x) + 1 + eps)) - 1) / (2 * eps))
+        ** 2
+        - 1
+    )
+
+
+def scalar_to_support(x, support_size):
+    x = _scaling(x)
+    x = jnp.clip(x, -support_size, support_size)
+    low = jnp.floor(x).astype(jnp.int32) 
+    high = jnp.ceil(x).astype(jnp.int32)
